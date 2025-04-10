7ed827b5453263a35a4bc31771e38b9c7e8139e0
krak <kraksin12@gmail.com>
2025-04-11 02:29:23 +0900

    fix: main 배포 시, vercel 돌아가지 않도록 변경

--- vercel-ignore-build-step.sh
+++ vercel-ignore-build-step.sh
@@ -1,4 +1,4 @@
-IGNORE_PATTERNS=("feat/" "feature/" "fix/" "chore/" "refactor/" "style/" "docs/" "test/")
+IGNORE_PATTERNS=("dev", "feat/" "feature/" "fix/" "chore/" "refactor/" "style/" "docs/" "test/")
 
 for pattern in "${IGNORE_PATTERNS[@]}"; do
   if [[ "$VERCEL_GIT_COMMIT_REF" == $pattern* ]]; then
