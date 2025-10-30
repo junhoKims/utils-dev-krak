015cef65d4b3e42d70f5558652d80de5514ebebd
krak <kraksin12@gmail.com>
2025-10-30 18:23:33 +0900

    chore: 수정된 biome 세팅에 의한 코드 정리
    - 코드 스타일 일관성 유지
    - 불필요한 파일 제거

--- src/common/slugify.ts
+++ src/common/slugify.ts
@@ -1,8 +1,8 @@
 export const slugify = (str: string) => {
-  return str
-    .toLowerCase()
-    .trim()
-    .replace(/[^\w\s-]/g, '')
-    .replace(/\s+/g, '-')
-    .replace(/^-+$/g, '');
+	return str
+		.toLowerCase()
+		.trim()
+		.replace(/[^\w\s-]/g, '')
+		.replace(/\s+/g, '-')
+		.replace(/^-+$/g, '');
 };
--- src/react/promise-settled-validator.tsx
+++ src/react/promise-settled-validator.tsx
@@ -1,15 +1,14 @@
-
 /**
  * `Promise.allSettled`의 결과를 선언적으로 작성할 수 있도록 구성한 유틸 함수
- * 
+ *
  * @description
  * - 결과가 성공(fulfilled)이라면 renderProps을 통해 성공된 promise타입을 리턴
  * - 결과가 실패(reject)라면 Error를 throw
- * 
+ *
  * @example
  * ```tsx
  * const [result1, result2] = await fetchPromiseAllSettled();
- * 
+ *
  * <ErrorBoundary fallbackRenderer={<Error />}>
  *   <Suspense fallback={<Loading />}>
  *     <PromiseAllSettledFulfilled result={result1}>
@@ -22,20 +21,18 @@
  * ```
  */
 export const PromiseAllSettledFulfilled = <T,>(
-  result: PromiseSettledResult<T>,
-  children: (result: PromiseFulfilledResult<T>) => React.ReactNode,
+	result: PromiseSettledResult<T>,
+	children: (result: PromiseFulfilledResult<T>) => React.ReactNode,
 ) => {
-  if (!isFulfilled(result)) {
-    throw new Error(result.reason ?? 'Promise Settled Rejected')
-  }
+	if (!isFulfilled(result)) {
+		throw new Error(result.reason ?? 'Promise Settled Rejected');
+	}
 
-  return (
-    <>{children(result)}</>
-  )
+	return <>{children(result)}</>;
 };
 
 const isFulfilled = <T,>(
-  result: PromiseSettledResult<T>,
+	result: PromiseSettledResult<T>,
 ): result is PromiseFulfilledResult<T> => {
-  return result.status === "fulfilled";
+	return result.status === 'fulfilled';
 };
