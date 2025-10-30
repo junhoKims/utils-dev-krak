3a5dcf74f9fc961b593646065dbb21a3620ba0ff
krak <kraksin12@gmail.com>
2025-10-30 17:31:42 +0900

    [BALCONY-29901] chore: 수정된 biome 세팅에 의한 코드 정리
    - 코드 스타일 일관성 유지
    - 불필요한 파일 제거

--- src/common/measureFPS.test.ts
+++ src/common/measureFPS.test.ts
@@ -1,9 +1,8 @@
-import { measureFPS } from "./measureFPS.js"
+import { measureFPS } from './measureFPS.js';
 
 describe('measureFPS', () => {
-  test('1초당 fps를 리턴한다', async () => {
-    const fps = await measureFPS();
-    expect(fps).toBeGreaterThan(55)
-    
-  })
-})  
+	test('1초당 fps를 리턴한다', async () => {
+		const fps = await measureFPS();
+		expect(fps).toBeGreaterThan(55);
+	});
+});
